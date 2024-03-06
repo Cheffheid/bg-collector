e4e99fac9ce85d51f8f1a9b27dfa44364deeb331
Jeffrey de Wit <jeffrey@webdevstudios.com>
2024-03-06 18:25:34 -0500

    Replace posts, with boardgame

--- src/app/_components/add-boardgame.tsx
+++ src/app/_components/add-boardgame.tsx
@@ -5,11 +5,11 @@ import { useState } from "react";
 
 import { api } from "~/trpc/react";
 
-export function CreatePost() {
+export function AddBoardgame() {
   const router = useRouter();
   const [name, setName] = useState("");
 
-  const createPost = api.post.create.useMutation({
+  const addBoardgame = api.boardgame.create.useMutation({
     onSuccess: () => {
       router.refresh();
       setName("");
@@ -20,7 +20,7 @@ export function CreatePost() {
     <form
       onSubmit={(e) => {
         e.preventDefault();
-        createPost.mutate({ name });
+        addBoardgame.mutate({ name });
       }}
       className="flex flex-col gap-2"
     >
@@ -34,9 +34,9 @@ export function CreatePost() {
       <button
         type="submit"
         className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
-        disabled={createPost.isLoading}
+        disabled={addBoardgame.isLoading}
       >
-        {createPost.isLoading ? "Submitting..." : "Submit"}
+        {addBoardgame.isLoading ? "Submitting..." : "Submit"}
       </button>
     </form>
   );
