be4192ab29987be05945e750225822bf55b9a030
Jeffrey de Wit <jeffrey@webdevstudios.com>
2024-03-07 12:14:24 -0500

    Clean up Boardgame TRPC router

--- src/server/api/routers/boardgame.ts
+++ src/server/api/routers/boardgame.ts
@@ -3,14 +3,6 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const boardgameRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
-
   create: publicProcedure
     .input(z.object({ name: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
@@ -27,9 +19,7 @@ export const boardgameRouter = createTRPCRouter({
       });
     }),
 
-  getLatest: publicProcedure.query(({ ctx }) => {
-    return ctx.db.boardgame.findFirst({
-      orderBy: { createdAt: "desc" },
-    });
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.db.boardgame.findMany();
   }),
 });
