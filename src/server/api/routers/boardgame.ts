50c44db7b5432ec36fc98e0e62abc54fda8f923e
Jeffrey de Wit <jeffrey@webdevstudios.com>
2024-03-07 18:10:26 -0500

    Update boardgame procedures with private procedures

--- src/server/api/routers/boardgame.ts
+++ src/server/api/routers/boardgame.ts
@@ -1,25 +1,42 @@
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 
 export const boardgameRouter = createTRPCRouter({
-  create: publicProcedure
+  create: privateProcedure
     .input(z.object({ name: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
-      const authorId = "userId";
-
       // simulate a slow db call
       await new Promise((resolve) => setTimeout(resolve, 1000));
 
       return ctx.db.boardgame.create({
         data: {
-          authorId,
+          authorId: ctx.currentUser,
           name: input.name,
         },
       });
     }),
 
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.db.boardgame.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const games = await ctx.db.boardgame.findMany({
+      take: 100,
+    });
+
+    return games;
+  }),
+
+  getAllFromUser: privateProcedure.query(async ({ ctx }) => {
+    const games = await ctx.db.boardgame.findMany({
+      take: 100,
+      where: {
+        authorId: ctx.currentUser,
+      },
+    });
+
+    return games;
   }),
 });
