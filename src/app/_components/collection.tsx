b473c35fc104773158b8db9f56b520de16253c3e
Jeffrey de Wit <jeffrey@webdevstudios.com>
2024-03-07 18:10:43 -0500

    Only load current user collection

--- src/app/_components/collection.tsx
+++ src/app/_components/collection.tsx
@@ -3,7 +3,7 @@
 import { api } from "~/trpc/react";
 
 export function Collection() {
-  const { data } = api.boardgame.getAll.useQuery();
+  const { data } = api.boardgame.getAllFromUser.useQuery();
 
   return <>{data?.map((game) => <div key={game.id}>{game.name}</div>)}</>;
 }
