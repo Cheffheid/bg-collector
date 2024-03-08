e82655e9784271d258f6ea962b9222f5382e0201
Jeffrey de Wit <jeffrey@webdevstudios.com>
2024-03-08 17:28:53 -0500

    Remove slow db call simulator

--- src/server/api/routers/boardgame.ts
+++ src/server/api/routers/boardgame.ts
@@ -10,9 +10,6 @@ export const boardgameRouter = createTRPCRouter({
   create: privateProcedure
     .input(z.object({ name: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-
       return ctx.db.boardgame.create({
         data: {
           authorId: ctx.currentUser,
