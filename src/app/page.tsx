c7d9cbfee4680b61c8eb888aef5b2d7939eb035a
Jeffrey de Wit <jeffrey@webdevstudios.com>
2024-03-08 17:29:00 -0500

    Move sign in button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,12 +15,10 @@ export default function Home() {
     <main className="min-h-screen bg-slate-100">
       <div className="flex px-2 py-1">
         <h1>Boardgame Collector</h1>
-        <div className="ml-auto">
-          {!user.isSignedIn && <SignInButton />}
-          {!!user.isSignedIn && <UserButton />}
-        </div>
+        <div className="ml-auto">{!!user.isSignedIn && <UserButton />}</div>
       </div>
       <div className="container mx-auto flex flex-col items-center justify-center gap-12 px-4 py-16">
+        {!user.isSignedIn && <SignInButton />}
         {!!user.isSignedIn && <AddBoardgame />}
         {!!user.isSignedIn && <Collection />}
       </div>
