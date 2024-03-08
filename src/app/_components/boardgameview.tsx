f597e267ab8fd2a9cc2facd424d9a23deb3e1a66
Jeffrey de Wit <jeffrey@webdevstudios.com>
2024-03-08 17:26:36 -0500

    Add a delete button for each game

--- src/app/_components/boardgameview.tsx
+++ src/app/_components/boardgameview.tsx
@@ -1,13 +1,32 @@
+import { api } from "~/trpc/react";
 import type { RouterOutputs } from "~/trpc/shared";
 
 type Boardgame = RouterOutputs["boardgame"]["getAll"][number];
 
 export const BoardgameView = (props: Boardgame) => {
   const { id, name } = props;
+
+  const ctx = api.useUtils();
+
+  const deleteBoardgame = api.boardgame.delete.useMutation({
+    onSuccess: () => {
+      void ctx.boardgame.getAllFromUser.invalidate();
+    },
+  });
+
   return (
     <div key={id} className="flex gap-3 border-b border-slate-400 p-4">
-      <div className="flex flex-col">
-        <div className="flex gap-1">{name}</div>
+      <div className="flex w-full justify-between">
+        {name}
+        <button
+          type="button"
+          className=""
+          onClick={() => {
+            deleteBoardgame.mutate({ id });
+          }}
+        >
+          X
+        </button>
       </div>
     </div>
   );
