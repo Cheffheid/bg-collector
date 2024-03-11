89268696709c837f47e3e2ff559f557425e4623c
Jeffrey de Wit <jeffrey@webdevstudios.com>
2024-03-11 18:40:28 -0400

    Input bg colour tweaks

--- src/app/_components/add-boardgame.tsx
+++ src/app/_components/add-boardgame.tsx
@@ -29,11 +29,11 @@ export function AddBoardgame() {
         placeholder="Add a game!"
         value={name}
         onChange={(e) => setName(e.target.value)}
-        className="grow px-4 py-2 text-black"
+        className="grow bg-slate-50 px-4 py-2 text-black transition focus:bg-white"
       />
       <button
         type="submit"
-        className="absolute right-0 px-10 py-2 font-semibold transition hover:bg-slate-600 hover:text-white"
+        className="absolute right-[1px] bg-slate-50 px-10 py-[6px] font-semibold transition hover:bg-slate-600 hover:text-white focus:bg-slate-600 focus:text-white"
         disabled={addBoardgame.isLoading}
       >
         {addBoardgame.isLoading ? "Submitting..." : "Add"}
