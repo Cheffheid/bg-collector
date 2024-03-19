187ad7ad9132b1974290a9356da526b53b3e1e40
Jeffrey de Wit <jeffrey@webdevstudios.com>
2024-03-18 20:01:30 -0400

    Emoji logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,8 +16,11 @@ export default function Home() {
   return (
     <main className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">
       <div className="flex h-screen flex-col justify-start">
-        <header className="flex items-center justify-between py-10 print:hidden">
-          <h1>Boardgame Collector</h1>
+        <header className="flex items-center justify-between py-10 text-4xl print:hidden">
+          <h1>
+            🎲
+            <span className="sr-only">Boardgame Collector</span>
+          </h1>
           <div className="ml-auto">{!!user.isSignedIn && <UserButton />}</div>
         </header>
         <div className="container mx-auto flex flex-col items-center divide-y divide-gray-200 dark:divide-gray-700">
