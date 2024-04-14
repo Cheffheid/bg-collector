53429b73e46835eab85bbfe22387d9672aee95aa
Jeffrey de Wit <jeffrey@webdevstudios.com>
2024-04-14 17:30:41 -0400

    Handle boardgame selection on Enter keypress when list item is selected

--- src/app/_components/boardgame/addBoardgame.tsx
+++ src/app/_components/boardgame/addBoardgame.tsx
@@ -1,27 +1,32 @@
-import React, { useState } from "react";
+import React, { type Dispatch, type SetStateAction, useState } from "react";
 
 import { api } from "~/trpc/react";
 
+const defaultGame = {
+  title: "",
+  yearPublished: 0,
+  id: 0,
+};
+
 export function AddBoardgame() {
-  const [name, setName] = useState("");
+  const [selectedGame, setSelectedGame] = useState(defaultGame);
 
   const ctx = api.useUtils();
 
   const addBoardgame = api.collection.addToCollection.useMutation({
     onSuccess: () => {
-      setName("");
+      setSelectedGame(defaultGame);
       void ctx.collection.getCollection.invalidate();
     },
   });
 
-  const handleInputChange = (e: React.FormEvent<HTMLInputElement>) => {
-    setName(e.currentTarget.value);
-  };
-
   return (
     <form className="relative mb-2 flex w-full flex-col pt-4 md:w-3/5">
       <div className="relative flex items-center justify-center pb-1">
-        <BoardgameInput onChangeCallback={handleInputChange} name={name} />
+        <BoardgameInput
+          selectedGame={selectedGame}
+          setSelectedGame={setSelectedGame}
+        />
         <button
           type="submit"
           className="absolute right-[1px] bg-slate-50 px-10 py-[6px] font-semibold transition hover:bg-slate-600 hover:text-white focus:bg-slate-600 focus:text-white"
@@ -35,34 +40,56 @@ export function AddBoardgame() {
 }
 
 const BoardgameInput = (props: {
-  onChangeCallback: (e: React.FormEvent<HTMLInputElement>) => void;
-  name: string;
+  selectedGame: {
+    title: string;
+    yearPublished: number;
+    id: number;
+  };
+  setSelectedGame: Dispatch<
+    SetStateAction<{ title: string; yearPublished: number; id: number }>
+  >;
 }) => {
   const [listHidden, setListHidden] = useState(true);
   const [activeIndex, setActiveIndex] = useState(-1);
   const [searchText, setSearchText] = useState("");
 
-  const { data: games } = api.boardgame.getGamesByTitle.useQuery({
+  let { data: games } = api.boardgame.getGamesByTitle.useQuery({
     title: searchText,
   });
 
+  if ("undefined" === typeof games) {
+    games = [];
+  }
+
   const handleKeydown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     const key = e.code;
-
-    if (!games || 0 === games.length) {
-      return;
-    }
-
-    if ("ArrowUp" !== key && "ArrowDown" !== key) {
-      return;
-    }
+    let preventDefault = false;
 
     if ("ArrowDown" === key) {
+      preventDefault = true;
       highlightNextItem();
     }
 
     if ("ArrowUp" === key) {
+      preventDefault = true;
       highlightPreviousItem();
+    }
+
+    if ("Enter" === key) {
+      preventDefault = true;
+
+      if (games && 0 < games.length && -1 !== activeIndex) {
+        const selectedGame = games.at(activeIndex);
+
+        if ("undefined" !== typeof selectedGame) {
+          props.setSelectedGame(selectedGame);
+          closeList();
+        }
+      }
+    }
+
+    if (preventDefault) {
+      e.preventDefault();
     }
   };
 
@@ -94,6 +121,11 @@ const BoardgameInput = (props: {
     setActiveIndex(newIndex);
   };
 
+  const closeList = () => {
+    setActiveIndex(-1);
+    setListHidden(true);
+  };
+
   return (
     <>
       <label htmlFor="boardgame-input" id="boardgame-label" className="sr-only">
@@ -103,16 +135,12 @@ const BoardgameInput = (props: {
         id="boardgame-input"
         type="text"
         placeholder="Add a game!"
-        value={props.name}
+        value={props.selectedGame.title}
         onChange={(e) => {
           setSearchText(e.currentTarget.value);
-          props.onChangeCallback(e);
         }}
         onFocus={() => setListHidden(false)}
-        onBlur={() => {
-          setActiveIndex(-1);
-          setListHidden(true);
-        }}
+        onBlur={closeList}
         onKeyDown={handleKeydown}
         role="combobox"
         aria-controls="boardgame-list"
